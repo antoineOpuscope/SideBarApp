9c5f20e64611e60d279b93e6ed6e578a0d985e2d
Antoine OMNES <antoine@minsar.com>
2023-08-30 10:39:45 +0200

    init user, company and address

---
+++ SideBarApp/Model/Company.swift
@@ -0,0 +1,14 @@
+//
+//  Company.swift
+//  SideBarApp
+//
+//  Created by Antoine Omnès on 30/08/2023.
+//
+
+import Foundation
+
+class Company: Codable {
+    var name: String
+    var catchPhrase: String
+    var bs: String
+}
---
+++ SideBarApp/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  SideBarApp
+//
+//  Created by Antoine Omnès on 30/08/2023.
+//
+
+import Foundation
+
+class User: Identifiable, Codable {
+    var id: Int
+    var username: String
+    var email: String
+    var address: Address
+    var phone: String
+    var website: String
+    var company: Company
+}
